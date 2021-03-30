1ac72ac9de6ad12770a73ee96bb2efdf7b94d357
Rahul7549 <rksharma15700@gmail.com>
2021-03-30 11:07:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formvalidation';
+  loginForm=new FormGroup({
+    email:new FormControl('',Validators.required),
+    phone:new FormControl('',Validators.required),
+    lname:new FormControl('',Validators.required),
+    fname:new FormControl('',Validators.required),
+    lamount:new FormControl('',Validators.required),
+    ltype:new FormControl('',Validators.required),
+    ldate:new FormControl('',Validators.required),
+    lrate:new FormControl('',Validators.required)
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports:[
+    BrowserModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
